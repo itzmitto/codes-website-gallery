8997589102d080f1cccfed5ff69c6981c428119c
Andre <andre.babirian2004@gmail.com>
2026-04-12 21:29:26 +0200

    66

---
+++ codes/25/script.js
@@ -0,0 +1,11 @@
+let display = document.getElementById("display");
+let memory = 0;
+
+function append(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
