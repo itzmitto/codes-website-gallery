968701233e8e8fd77d0510f68fd9c195d8114263
Andre <andre.babirian2004@gmail.com>
2026-04-12 21:31:21 +0200

    67

--- codes/25/script.js
+++ codes/25/script.js
@@ -9,3 +9,19 @@ function clearDisplay() {
     display.value = "";
 }
 
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }   
+}
+function sqrt() {
+        display.value = Math.sqrt(eval(display.value));
+    } 
+
+    function power() {
