80a5953afc46d82228a37614857e85d2461e3276
itzmitto <andre.babirian2004@gmail.com>
2026-05-13 13:07:41 +0200

    189

---
+++ codes/4/script.js
@@ -0,0 +1,29 @@
+const slider = document.getElementById("slider");
+const lengthVal = document.getElementById("lengthVal");
+const output = document.getElementById("output");
+
+slider.oninput = () => {
+  lengthVal.textContent = slider.value;
+};
+
+function generatePassword() {
+  let chars = "";
+
+  if (upper.checked) chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (lower.checked) chars += "abcdefghijklmnopqrstuvwxyz";
+  if (numbers.checked) chars += "0123456789";
+  if (symbols.checked) chars += "!@#$%^&*";
+
+  if (!chars) {
+    output.textContent = "Selecteer iets!";
+    return;
+  }
+
+  let password = "";
+
+  for (let i = 0; i < slider.value; i++) {
+    password += chars[Math.floor(Math.random() * chars.length)];
+  }
+
+  output.textContent = password;
+}
