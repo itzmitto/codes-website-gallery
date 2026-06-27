5e7a74bdeacab65917c3e6d30be922bb5367f1b7
itzmitto <andre.babirian2004@gmail.com>
2026-06-27 21:28:46 +0200

    html en css en js opdracht website afgemaakt

--- codes/112/main.js
+++ codes/112/main.js
@@ -26,18 +26,29 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 const swiperHome = new Swiper('.home__swiper', {
-  loop: true,
-  effect: 'fade',
-  parallax: true,
-  speed: 1000,
-  grabCursor: true,
+   loop: true,
+   effect: 'fade',
+   parallax: true,
+   speed: 1000,
+   grabCursor: true,
 
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+   pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+   },
    autoplay: {
       delay: 5000,
       disableOnInteraction: false,
    }
 })
+
+
+
+gsap.defaults({ opacity: 0, ease: 'power4.out', duration: 1.4, })
+
+const tl = gsap.timeline()
+tl.from('.home__img', { y: 200, stagger: 0.15 }, '.3')
+   .from('.nav > *', { y: -30 }, '.9')
+   .from('.home__data', { y: 60 }, '1.2')
+   .from('.home__info', { y: 100 }, '1.5')
+   .from('.home .swiper-pagination', { scale: 0, opacity: 1 }, '1.5')
