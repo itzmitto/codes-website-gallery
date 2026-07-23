99fd96c3737410ef65283ca0b28b4fc9408461f8
itzmitto <andre.babirian2004@gmail.com>
2026-07-23 21:25:01 +0200

    styling verbeterd

--- codes/139/script.js
+++ codes/139/script.js
@@ -1,8 +1,14 @@
 const website = document.getElementById("website");
-const links = document.querySelectorAll("nav a");
+const links = document.querySelectorAll(".navbar nav a");
+const revealButton = document.getElementById("revealButton");
+
+function toggleLoading(e) {
+    e.preventDefault();
+    website.classList.toggle("loading");
+}
+
 links.forEach(link => {
-    link.addEventListener("click", e => {
-        e.preventDefault();
-        website.classList.remove("loading");
-    });
+    link.addEventListener("click", toggleLoading);
 });
+
+revealButton.addEventListener("click", toggleLoading);
